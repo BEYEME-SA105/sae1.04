ef3f5fdd0efcea49d8f343090b61eef2fcff683d
gloire datchin <mboutagloire50@gmail.com>
2025-01-19 01:06:04 +0100

    Ajout des modifications et nouveaux fichiers

--- src/Controller/JenathanController.php
+++ src/Controller/JenathanController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class JenathanController extends AbstractController
 {
@@ -13,23 +13,28 @@ final class JenathanController extends AbstractController
     {
         return $this->render('jenathan/index.html.twig');
     }
-    
+
     #[Route('/portfolio', name: 'app_portfolio', methods: ['GET'])]
     public function portfolio(): Response
     {
         return $this->render('jenathan/portfolio.html.twig');
     }
-    
+
     #[Route('/competences', name: 'app_competences', methods: ['GET'])]
     public function competences(): Response
     {
         return $this->render('jenathan/competences.html.twig');
     }
-    
+
     #[Route('/contacter', name: 'app_contact', methods: ['GET'])]
     public function contacter(): Response
     {
         return $this->render('jenathan/contacter.html.twig');
     }
-}
 
+    #[Route('/loisirs de Jenathan', name: 'app_loisir', methods: ['GET'])]
+    public function loisir(): Response
+    {
+        return $this->render('jenathan/loisir.html.twig');
+    }
+}
